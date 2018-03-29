d5c8561965d64243c8f9dfcf8c405cc54b9c414c
BaoluoFeng <baoluofe@usc.edu>
2018-03-28 20:44:55 -0700

    second try

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://335.185.202.130:9200"
+	ES_URL = "http://35.185.202.130:9200"
 )
 
 func main() {
